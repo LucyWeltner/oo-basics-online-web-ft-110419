d0468fa2169ad22c28eee3754919d9d83ce57893
Lucy Weltner <lucyiweltner@gmail.com>
2019-11-11 00:23:52 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,9 +1,10 @@
 class Book
-  attr_accessor :title, :author, :page_count
+  attr_accessor :title, :author, :page_count, :genre
   @@genres = []
-  def initialize(genre, title, author)
+  def initialize(genre, title, author, page_count)
     @genre = genre 
     @title = title 
     @author = author 
+    @page_count = page_count
     @@genres << self.genre
   end 
