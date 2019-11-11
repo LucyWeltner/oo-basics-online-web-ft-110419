bb2994020447981f61a21b034f5e6b6bf0f3029d
Lucy Weltner <lucyiweltner@gmail.com>
2019-11-11 00:22:57 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,9 @@
-
-
+class Book
+  attr_accessor :title, :author, :page_count
+  @@genres = []
+  def initialize(genre, title, author)
+    @genre = genre 
+    @title = title 
+    @author = author 
+    @@genres << self.genre
+  end 
