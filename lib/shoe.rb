73159b1d7b4fd780e413744c864b9555772348a3
Lucy Weltner <lucyiweltner@gmail.com>
2019-11-11 00:34:04 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,7 +1,10 @@
 class Shoe 
-  attr_accessor :brand, :size, :material
+  attr_accessor :brand, :size, :material, :color, :condition
   def initialize(brand)
     @brand = brand
   end
-  
+  def cobble
+    puts "Your shoe is as good as new"
+  end 
+end 
   # Make your shoe class here!
