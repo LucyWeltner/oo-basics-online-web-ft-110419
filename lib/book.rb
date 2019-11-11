f8bc8f9600c641862fb2f44571125efdb1310d10
Lucy Weltner <lucyiweltner@gmail.com>
2019-11-11 00:38:12 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -15,4 +15,13 @@ class Book
   def turn_page
     puts "Flipping the page...wow, you read fast!"
   end 
+  def self.genres
+    @@genres 
+  end
 end 
+
+"Imaginary Friend" = Book.new 
+"Ill Will" = Book.new 
+"Inherent Vice" = Book.new 
+"Hobo Mom" = Book.new
+p Book.genres
