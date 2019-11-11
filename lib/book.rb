4735c18b64e49e57a856e0ca67f080ba16bdc73a
Lucy Weltner <lucyiweltner@gmail.com>
2019-11-11 00:32:04 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,9 +1,15 @@
 class Book
-  attr_accessor :title, :author, :page_count, :genre
+  attr_accessor :title, :author, :page_count
   @@genres = []
   def initialize(title)
     @title = title 
+  end 
+  def genre=(genre)
+    @genre = genre
     @@genres << self.genre
+  end
+  def genre
+    @genre
   end 
   def turn_page
     puts "Flipping the page...wow, you read fast!"
